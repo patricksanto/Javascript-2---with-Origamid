2624619e6cc14170a9a1c25a505822bcdfdb4126
Patrick Santos <patrick.sg.santos@gmail.com>
2022-12-02 19:55:38 -0300

    some string methods and trainning

--- arrays.js
+++ arrays.js
@@ -57,19 +57,19 @@
 // const li = new Dom("li");
 
 // Transforme o objeto abaixo em uma Constructor Function
-function pessoa(nome, idade) {
-  this.nome = nome;
-  this.idade = idade;
-  this.andar = function () {
-    console.log(this.nome + " andou");
-  };
-}
+// function pessoa(nome, idade) {
+//   this.nome = nome;
+//   this.idade = idade;
+//   this.andar = function () {
+//     console.log(this.nome + " andou");
+//   };
+// }
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
-const joao = new pessoa("Joao", 20);
-const maria = new pessoa("Maria", 25);
-const bruno = new pessoa("Bruno", 15);
+// const joao = new pessoa("Joao", 20);
+// const maria = new pessoa("Maria", 25);
+// const bruno = new pessoa("Bruno", 15);
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -97,18 +97,130 @@ const bruno = new pessoa("Bruno", 15);
 //   };
 // }
 
-function Dom2(seletor) {
-  const elementList = document.querySelectorAll(seletor);
-  this.elements = elementList;
-  this.addClass = function (classe) {
-    elementList.forEach((elemento) => {
-      elemento.classList.add(classe);
-    });
+// function Dom2(seletor) {
+//   const elementList = document.querySelectorAll(seletor);
+//   this.elements = elementList;
+//   this.addClass = function (classe) {
+//     elementList.forEach((elemento) => {
+//       elemento.classList.add(classe);
+//     });
+//   };
+//   this.removeClass = function (classe) {
+//     elementList.forEach((elemento) => {
+//       elemento.classList.remove(classe);
+//     });
+//   };
+// }
+// const li = new Dom2("li");
+
+const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const comidasPrimeiro = comidas.shift();
+// Remova o último valor de comidas e coloque em uma variável
+const comidasultimo = comidas.pop();
+// Adicione 'Arroz' ao final da array
+comidas.push("Arroz");
+console.log(comidas);
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift("Peixe", "Batata");
+console.log(comidas);
+
+const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
+// Arrume os estudantes em ordem alfabética
+console.log(estudantes.sort());
+// Inverta a ordem dos estudantes
+console.log(estudantes.reverse());
+// Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes("Joana"));
+// Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes("Juliana"));
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`;
+// Substitua section por ul e div com li,
+// utilizando split e join
+// console.log(html.split("section").join("ul").split("div").join("li"));
+
+// const carros = ["Ford", "Fiat", "VW", "Honda"];
+// // Remova o último carro, mas antes de remover
+// // salve a array original em outra variável
+// const carros2 = carros.slice();
+// carros.pop();
+// console.log(carros2);
+// console.log(carros);
+
+// const numeros = [2, 4, 56, 76, 4, 23, 876, 98, 45, 32, 45];
+
+// const numeroMaior = numeros.reduce(
+//   (anterior, atual) => (anterior > atual ? anterior : atual),
+//   0
+// );
+// console.log(numeroMaior);
+
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll(".curso");
+const arrayCursos = Array.from(cursos);
+const objetoCurso = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aulas = curso.querySelector(".aulas").innerText;
+  const horas = curso.querySelector(".horas").innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
   };
-  this.removeClass = function (classe) {
-    elementList.forEach((elemento) => {
-      elemento.classList.remove(classe);
-    });
-  };
-}
-const li = new Dom2("li");
+});
+console.log(objetoCurso);
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQueCem = numeros.filter((x) => x > 100);
+console.log(maioresQueCem);
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+console.log(
+  instrumentos.some((n) => {
+    return n === "Baixo";
+  })
+);
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: "Banana",
+    preco: "R$ 4,99",
+  },
+  {
+    item: "Ovo",
+    preco: "R$ 2,99",
+  },
+  {
+    item: "Carne",
+    preco: "R$ 25,49",
+  },
+  {
+    item: "Refrigerante",
+    preco: "R$ 5,35",
+  },
+  {
+    item: "Quejo",
+    preco: "R$ 10,60",
+  },
+];
+
+const valorTotal = compras.reduce((acumulador, item) => {
+  const precoLimpo = +item.preco.replace("R$ ", "").replace(",", ".");
+  return acumulador + precoLimpo;
+}, 0);
+
+console.log(valorTotal);
--- strings.js
+++ strings.js
@@ -66,3 +66,20 @@ transacoes2.forEach((tran) => {
   tran = tran.trim();
   console.log(tran);
 });
+
+const aleatorio = Math.random() * 10;
+console.log(aleatorio.toFixed());
+
+// Retorne um número aleatório
+// entre 1050 e 2000
+
+const aleatorio2 = Math.random() * (2000 - 1050 + 1) + 1050);
+console.log(aleatorio2);
+
+// Retorne o maior número da lista abaixo
+const numeros = "4, 5, 20, 8, 9";
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
